6d5239b9f2a92ea2820247b51144660b02b79bf1
Arthur Mengual <arthur.mengual@gmail.com>
2021-09-27 09:32:57 +0200

    caroussel + modale ok

---
+++ apart.js
@@ -0,0 +1,41 @@
+
+//function to fetch films on a single page in the api
+
+async function get_film(genre, page) {
+    const response = await fetch(entry_point + '?genre=' + genre + '&page=' + page);
+    const data = await response.json();
+    return data.results
+}
+
+//function to fectch 10 films il the api, on two different pages
+
+
+entry_point = "http://localhost:8000/api/v1/titles/"
+
+let caroussel_tags = document.getElementsByClassName('img_film');
+
+
+
+async function get_ten_films(genre){
+    const films_1 = await get_film(genre, 1)
+    const films_2 = await get_film(genre, 2)
+    const all_films = await films_1.concat(films_2)
+    return all_films
+}
+
+//function to display films images et infos in the caroussel, also stores the film id
+
+async function display_films_imgs(genre) {
+    const films = await get_ten_films(genre);
+    try {
+        for (i = 0; i < 7; i++) {
+            caroussel_tags[i].src = films[i].src
+        }
+    } catch (err) { console.log(error); }
+    
+}
+
+
+    
+    
+            
--- script.js
+++ script.js
@@ -1,103 +1,76 @@
-const entry_point = "http://localhost:8000/api/v1/titles/"
-
-var search = document.getElementById("search_logo");
-
-var input_search = document.getElementById("input");
-
-search.onclick = function() {
-    input_search.style.display = 'block'
-}
+url = 'http://localhost:8000/api/v1/titles/'
 
 
 
-/* modales main film */
-
-var main_film = document.getElementById("main_film_link")
-
-var modale = document.getElementById("modale_content")
-
-var close_btn = document.getElementById("close")
 
 
-main_film.onclick = function (){
-    modale.style.display = 'block'
-}
-
-close_btn.onclick = function () {
-    modale.style.display = "none"
-}
+/***MAIN FILM */
+fetch(url)
+    .then(res => res.json())
+    .then(data=>{document.querySelector('#main_film').src = data.results[0].image_url})
 
 
-/*fonction create modal*/
 
 
-// function modal(film_id) {
-// //     /*search in server data corresponding infos*/
-// //     /*create the modal*/
-// //     /*display modal*/
-// //     /* close modal*/
-//     fetch(entry_point + id)
-
-//  }
 
 
-/*CAROUSEL*/
 
-var films = document.getElementsByClassName("img_film")
+/****CAROUSSEL PART */
 
-function film(film, i) {
-    fetch(entry_point)
+
+
+//get films infos
+function get_movies(genre) {
+    fetch(url + '?genre=' + genre + '&page=' + 1)
         .then(res => res.json())
-        .then(data => film.src = data.results[i].image_url)
+        .then(data => {
+            for (let i = 0; i < 4; i++) {
+                document.querySelectorAll('.img_film')[i].src = data.results[i].image_url
+            }
+        }
+        )
 }
+            
+get_movies('Action')
 
-for (let i in films) {
-    film(films[i], i);
-    i++
-}
 
-//event listener to open modal
 
-for (let i in films) {
-    films[i].onclick = function (){
-        modale.style.display = 'block'
+
+
+
+/******MODALE PART */
+
+//create and display modale
+function create_modal(film_id) {
+    fetch(url + film_id)
+        .then(res => res.json())
+        .then(data => {
+            const title = data.title;
+            const genre = data.genre;
+            const img = data.image_url;
+            document.querySelector('#modale_content').style.background = img
+        })
+        .then(document.querySelector('#modale_content').style.display = 'block')
+ }
+
+
+// display modale
+//caroussel
+const films = document.querySelectorAll('.img_film')
+for (let i = 0; i < 4; i++) {
+    films[i].onclick = function () {
+        create_modal(499549);
     }
 }
 
-//fetch autant de pic que de balises
-
-// var pix = [];
-// let i = 1;
-
-// while (pix.length < films.length) {
-//     console.log(i, pix)
-//     if (i == 1) {
-//         fetch(entry_point)
-//             .then(res => res.json())
-//             .then(data => data.results.forEach(item => pix.push(item)))
-//     } else {
-//         fetch(entry_point + '?page=' + i)
-//             .then(res => res.json())
-//             .then(data => data.results.forEach(item => pix.push(item)))
-//     }
-//     i++;
-// }
-// console.log(pix)
+//main film
+document.querySelector('#main_film').onclick = function () {
+    create_modal(499549);
+}
 
 
-
-
-
-
-
-
-
-
-
-//creer une fonction get categories film
-
-//liste avec mes 7 images de l'api
-
-//stocker les id de films dans id de la balise html
-
-//laisser structure html pour la modale et remplacer les elt avec une fonction js
+//close modale
+const close = document.querySelector('#close');
+close.onclick = function () {
+    document.querySelector('#modale_content').style.display = 'none'
+}

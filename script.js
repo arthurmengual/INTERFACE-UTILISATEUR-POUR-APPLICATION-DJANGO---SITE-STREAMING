8307bca834533e30a416443bae17db9a9ef2655f
Arthur Mengual <arthur.mengual@gmail.com>
2021-10-09 11:57:16 +0200

    all is working

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-url = 'http://localhost:8000/api/v1/titles/'
+var url = 'http://localhost:8000/api/v1/titles/';
 
 
 /***MAIN FILM */
-
+var films_images = {}
 
 async function displayMainFilm(url){
-    const response = await fetch(url);
+    const response = await fetch(url + '?sort_by=imdb_score');
     const data = await response.json();
     const main_film = document.querySelector('.main');
-    main_film.src = data.results[0].image_url;
-    main_film.id = data.results[0].id;
+    main_film.src = data.results[1].image_url;
+    main_film.id = data.results[1].id;
 }
 
 /****CAROUSSEL PART */
 
 //function to get movie on a single page of the api
-async function get_film(filtre, page) {
-    const response = await fetch(url + filtre + '&page=' + page);
+async function get_film(genre, page) {
+    const response = await fetch(url + genre + '&sort_by=imdb_score' + '&page=' + page);
     const data = await response.json();
-    return data.results
+    return data.results;
 }
 
 
 //function to fectch films on two pages of the api
-async function get_seven_films(filtre){
-    const films_1 = await get_film(filtre, 1);
-    const films_2 = await get_film(filtre, 2);
+async function get_seven_films(genre){
+    const films_1 = await get_film(genre, 1);
+    const films_2 = await get_film(genre, 2);
     const all_films = await films_1.concat(films_2);
     for (let film of all_films) {
-        if (!film.image_url) {all_films.splice(film, 1)}
+        if (!film.image_url) {all_films.splice(film, 1);}
     }
-    return all_films.slice(0,7)
+    films_images[genre] = all_films.slice(0, 7);
+    return all_films.slice(0,7);
 }
 
 
 //function to create a caroussel
-async function create_caroussel(genre) {
-    movies = await get_seven_films(`?genre=${genre}`);
-    var div_container = document.createElement('container');
-    document.body.appendChild(div_container);
+async function create_caroussel(genre, car_nb) {
+    var movies = await get_seven_films(`?genre=${genre}`);
+    var div_container = document.getElementById(`caroussel_${car_nb}`)
     var title = document.createElement('h2');
     title.textContent = genre;
-    div_container.appendChild(title)
+    if (genre == '') {title.textContent = 'FILMS LES MIEUX NOTES';}
+    div_container.appendChild(title);
     var div_caroussel = document.createElement('div');
     div_caroussel.setAttribute('class', 'caroussel');
     div_caroussel.setAttribute('id', `caroussel ${genre}`);
@@ -51,24 +52,20 @@ async function create_caroussel(genre) {
     btn_l.setAttribute('id', `btn_left_${genre}`);
     btn_l.textContent = '<';
     div_caroussel.appendChild(btn_l);
-    let i = 0;
-    for (movie of movies) {
+    for (let i = 0; i < 4; i++) {
         var div_item = document.createElement(('div'));
         div_item.setAttribute('class', `item${genre}`);
+        div_item.setAttribute('class', 'item');
         div_caroussel.appendChild(div_item);
         var a = document.createElement('a');
         a.setAttribute('href', '#');
-        a.setAttribute('class', 'img')
+        a.setAttribute('class', 'img');
         var image = document.createElement('img');
         image.setAttribute('class', `img_caroussel${genre}`);
-        image.setAttribute('id', movie.id);
-        image.setAttribute('src', movie.image_url);
+        image.setAttribute('id', movies[i].id);
+        image.setAttribute('src', movies[i].image_url);
         div_item.appendChild(a);
         a.appendChild(image);
-        if (i > 4) {
-            div_item.style.display = 'none'
-        }
-        i++;
     }
     var btn_r = document.createElement('a');
     btn_r.setAttribute('class', 'btn_right');
@@ -80,29 +77,26 @@ async function create_caroussel(genre) {
 
 
 //scroll caroussel
-var DicoIndex = {
-    IndexHideAction: 0, IndexShowAction: 4, IndexHideComedy: 0, IndexShowComedy: 4,
-    IndexHideFamily: 0, IndexShowFamily: 4, IndexHideC4: 0, IndexShowC4: 4}
+var DicoIndex = { Index: 0, IndexAction: 0, IndexComedy: 0, IndexFamily: 0 };
 
-function scroll_forward(genre, IndexHide, IndexShow, MoviesSelectors) {
-    IndexHide = (IndexHide + 1) % 7;
-    DicoIndex[`IndexHide${genre}`] = IndexHide;
-    IndexShow = (IndexShow + 1) % 7;
-    DicoIndex[`IndexShow${genre}`] = IndexShow;
-    MoviesSelectors[IndexHide].style.display = 'none';
-    MoviesSelectors[IndexShow].style.display = 'block'    
+
+function scroll_forward(genre, index, MoviesSelectors) {
+    var movies_data = films_images[`?genre=${genre}`]
+    index = index + 1;
+    DicoIndex[`Index${genre}`] = index
+    for (let i = 0; i < 4; i++){
+        MoviesSelectors[i].src = movies_data[(index+i)%7].image_url;
+    }
 }
 
-function scroll_backward(genre, IndexHide, IndexShow, MoviesSelectors) {
-    IndexHide = (IndexHide - 1 + 7) % 7;
-    DicoIndex[`IndexHide${genre}`] = IndexHide;
-    IndexShow = (IndexShow - 1 + 7) % 7;
-    DicoIndex[`IndexShow${genre}`] = IndexShow;
-    MoviesSelectors[IndexHide].style.display = 'none';
-    MoviesSelectors[IndexShow].style.display = 'block'
-
+function scroll_backward(genre, index, MoviesSelectors) {
+    var movies_data = films_images[`?genre=${genre}`]
+    index = (index - 1+7)%7;
+    DicoIndex[`Index${genre}`] = index
+    for (let i = 0; i < 4; i++){
+        MoviesSelectors[i].src = movies_data[(index+i)%7].image_url;
+    }
 }
-
 
 /******MODALE PART */
 
@@ -121,7 +115,7 @@ function create_modal(film_id) {
             ${data.countries} <br/> <strong>B ox office: </strong> &nbsp; &nbsp; ${data.reviews_from_users} <br/> <strong> Description: </strong>
             &nbsp; &nbsp; ${data.description}`;
         })
-        .then(document.querySelector('#modale_content').style.display = 'flex')
+        .then(document.querySelector('#modale_content').style.display = 'flex');
  }
 
 
@@ -133,16 +127,17 @@ async function main_film(){
     const id = main.id;
     main.onclick = function () {
         create_modal(id);
-    }
+    };
 }
 
-main_film()
+main_film();
+
 
 //close btn modale
 const close = document.querySelector('#close');
 close.onclick = function () {
-    document.querySelector('#modale_content').style.display = 'none'
-}
+    document.querySelector('#modale_content').style.display = 'none';
+};
 
 
 
@@ -150,26 +145,32 @@ close.onclick = function () {
 
 
 //Display caroussels and set buttns
-async function display_caroussel(genre) {
-    await create_caroussel(genre);
+async function display_caroussel(genre, car_nb) {
+    await create_caroussel(genre, car_nb);
     var btnR = document.getElementById(`btn_right_${genre}`);
     var btnL = document.getElementById(`btn_left_${genre}`);
-    var selectors = document.getElementsByClassName(`item${genre}`);
-    btnR.onclick = scroll_forward(genre, DicoIndex[`IndexHide${genre}`],
-        DicoIndex[`IndexShow${genre}`], selectors);
-    btnL.onclick = scroll_backward(genre, DicoIndex[`IndexHide${genre}`],
-        DicoIndex[`IndexShow${genre}`], selectors);
+    var selectors = document.getElementsByClassName(`img_caroussel${genre}`);
+    btnR.onclick = function () {
+        scroll_forward(genre, DicoIndex[`Index${genre}`], selectors);
+    };
+    btnL.onclick = function () {
+        scroll_backward(genre, DicoIndex[`Index${genre}`], selectors);
+    };
     
     const films = document.getElementsByClassName(`img_caroussel${genre}`);
     for (let film of films) {
         film.onclick = function () {
-            let id = film.id
+            let id = film.id;
             create_modal(id);
-        }   
-    }   
+        } ;  
     }
-
-const genres = ['Action', 'Family', 'Comedy']
-for (let genre of genres){
-    display_caroussel(genre)
 }
+    
+//display every caroussels
+let i = 1;
+const genres = ['', 'Action', 'Family', 'Comedy'];
+for (let genre of genres){
+    display_caroussel(genre, i);
+    i++;
+}
+
